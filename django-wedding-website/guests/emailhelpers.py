96e3c36c3fc33153d7bec4a1f0f93fa36294c20a
mikelaughton2 <michael.laughton@gmail.com>
2023-01-02 12:03:35 +0000

    Fix site password error for translated sites.

--- django-wedding-website/guests/emailhelpers.py
+++ django-wedding-website/guests/emailhelpers.py
@@ -3,7 +3,7 @@ def get_site_password():
     #Return the site passsword or none (handle none in template)
     #Relies on there being a page with the slug 'home'
     try:
-        hp_pwd = HomePage.objects.get(slug='home')[0]
+        hp_pwd = HomePage.objects.filter(slug='home')[0]
         return hp_pwd.get_view_restrictions().values_list('password',flat=True)[0]
     except HomePage.DoesNotExist:
         return ''
